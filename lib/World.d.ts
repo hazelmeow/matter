c574b3b7e6e80ec0299c46ed4481ce6285f289ca
hazel <hazel@meows.zip>
2023-07-23 14:51:23 -0700

    Add entity brand generic to World

--- lib/World.d.ts
+++ lib/World.d.ts
@@ -1,24 +1,28 @@
 import { AnyComponent, ComponentBundle, ComponentCtor, DynamicBundle, InferComponents } from "./component";
 
+export type Brand<B extends string> = {
+	readonly __entity_brand: B;
+}
+
 /**
  * Nominal type to describe an entity with an expressed set of associated components.
  * @typeParam T - Bundle of component values
  */
 
-export type Entity<T extends ComponentBundle> = number & {
+export type Entity<T extends ComponentBundle, B extends string> = number & {
 	/**
 	 * @hidden
 	 */
 	readonly __nominal_entity: T;
-};
+} & Brand<B>;
 
-export type GenericOfEntity<T> = T extends Entity<infer a> ? a : never;
+export type GenericOfEntity<T, B extends string> = T extends Entity<infer a, B> ? a : never;
 
 /**
  * AnyEntity is a plain number, and can be used as such or be casted back and forth, however it upholds a type contract that prevents accidental misuse by enforcing
  * developers to think about what they really wanted to use.
  */
-export type AnyEntity = Entity<ComponentBundle>;
+export type AnyEntity<B extends string> = Entity<ComponentBundle, B>;
 
 type Equals<A1, A2> = (<A>() => A extends A2 ? 1 : 0) extends <A>() => A extends A1 ? 1 : 0 ? 1 : 0;
 
@@ -41,9 +45,9 @@ type NullableArray<A extends Array<unknown>> = Partial<A> extends Array<unknown>
  * Entities are simply ever-increasing integers.
  */
 
-export interface World extends IterableFunction<LuaTuple<[AnyEntity, Map<ComponentCtor, AnyComponent>]>> {}
+export interface World<B> extends IterableFunction<LuaTuple<[AnyEntity<B>, Map<ComponentCtor, AnyComponent>]>> {}
 
-export class World {
+export class World<B extends string> {
 	public constructor();
 
 	/**
@@ -51,7 +55,7 @@ export class World {
 	 * @param component_bundle - The component values to spawn the entity with.
 	 * @return The new entity ID.
 	 */
-	public spawn<T extends ComponentBundle>(...component_bundle: T): Entity<T>;
+	public spawn<T extends ComponentBundle>(...component_bundle: T): Entity<T, B>;
 
 	/**
 	 * Spawns a new entity in the world with a specific entity ID and given components.
@@ -60,7 +64,7 @@ export class World {
 	 * @param component_bundle - The component values to spawn the entity with.
 	 * @see {@link id Entity}
 	 */
-	public spawnAt<T extends ComponentBundle>(id: number, ...component_bundle: T): Entity<T>;
+	public spawnAt<T extends ComponentBundle>(id: number, ...component_bundle: T): Entity<T, B>;
 	/**
 	 * Replaces a given entity by ID with an entirely new set of components.
 	 * Equivalent to removing all components from an entity, and then adding these ones.
@@ -68,14 +72,14 @@ export class World {
 	 * @param component_bundle {@link ComponentBundle ComponentBundle} - The component values to spawn the entity with.
 	 * @see {@link id AnyEntity}
 	 */
-	public replace<T extends ComponentBundle>(id: AnyEntity, ...component_bundle: T): Entity<T>;
+	public replace<T extends ComponentBundle>(id: AnyEntity<B>, ...component_bundle: T): Entity<T, B>;
 
 	/**
 	 * Despawns a given entity by ID, removing it and all its components from the world entirely.
 	 * @param id - The entity ID
 	 * @see {@link id AnyEntity}
 	 */
-	public despawn(id: AnyEntity): void;
+	public despawn(id: AnyEntity<B>): void;
 
 	/**
 	 * Removes all entities from the world.
@@ -91,7 +95,7 @@ export class World {
 	 * @returns boolean - `true` if the entity exists
 	 * @see {@link AnyEntity AnyEntity}
 	 */
-	public contains(id: AnyEntity): boolean;
+	public contains(id: AnyEntity<B>): boolean;
 
 	/**
 	 * Gets a specific component from a specific entity in this world.
@@ -102,10 +106,10 @@ export class World {
 	 * @remarks
 	 * Component values returned are nullable if the components used to search for aren't associated with the entity (in real-time).
 	 */
-	public get<a extends AnyEntity, T extends ComponentCtor>(
+	public get<a extends AnyEntity<B>, T extends ComponentCtor>(
 		entity: a,
 		only: T,
-	): Includes<GenericOfEntity<a>, ReturnType<T>> extends true ? ReturnType<T> : ReturnType<T> | undefined;
+	): Includes<GenericOfEntity<a, B>, ReturnType<T>> extends true ? ReturnType<T> : ReturnType<T> | undefined;
 
 	/**
 	 * Gets a specific set of components from a specific entity in this world.
@@ -116,10 +120,10 @@ export class World {
 	 * @remarks
 	 * Component values returned are nullable if the components used to search for aren't associated with the entity (in real-time).
 	 */
-	public get<a extends AnyEntity, T extends DynamicBundle>(
+	public get<a extends AnyEntity<B>, T extends DynamicBundle>(
 		entity: a,
 		...bundle: T
-	): LuaTuple<a extends Entity<InferComponents<T>> ? InferComponents<T> : NullableArray<InferComponents<T>>>;
+	): LuaTuple<a extends Entity<InferComponents<T>, B> ? InferComponents<T> : NullableArray<InferComponents<T>>>;
 
 	/**
 	 * Performs a query against the entities in this World. Returns a [QueryResult](/api/QueryResult), which iterates over
@@ -129,24 +133,24 @@ export class World {
 	 * @param dynamic_bundle
 	 * @returns QueryResult - See {@link QueryResult QueryResult}
 	 */
-	public query<T extends DynamicBundle, a extends InferComponents<T>>(...dynamic_bundle: T): QueryResult<a>;
+	public query<T extends DynamicBundle, a extends InferComponents<T>>(...dynamic_bundle: T): QueryResult<a, B>;
 
 	public queryChanged<C extends ComponentCtor>(
 		mt: C,
 	): IterableFunction<
-		LuaTuple<[Entity<[ReturnType<C>]>, { new: ReturnType<C> | undefined; old: ReturnType<C> | undefined }]>
+		LuaTuple<[Entity<[ReturnType<C>], B>, { new: ReturnType<C> | undefined; old: ReturnType<C> | undefined }]>
 	>;
 
-	public insert(id: AnyEntity, ...dynamic_bundle: ComponentBundle): void;
+	public insert(id: AnyEntity<B>, ...dynamic_bundle: ComponentBundle): void;
 
-	public remove<T extends DynamicBundle>(id: AnyEntity, ...dynamic_bundle: T): LuaTuple<InferComponents<T>>;
+	public remove<T extends DynamicBundle>(id: AnyEntity<B>, ...dynamic_bundle: T): LuaTuple<InferComponents<T>>;
 
 	public size(): number;
 
 	public optimizeQueries(): void;
 }
 
-type Query<T extends ComponentBundle> = IterableFunction<LuaTuple<[Entity<T>, ...T]>>;
+type Query<T extends ComponentBundle, B extends string> = IterableFunction<LuaTuple<[Entity<T, B>, ...T]>>;
 
 /**
  * @class QueryResult
@@ -157,7 +161,7 @@ type Query<T extends ComponentBundle> = IterableFunction<LuaTuple<[Entity<T>, ..
  * Calling the table or the `next` method allows iteration over the results. Once all results have returned, the
  * QueryResult is exhausted and is no longer useful
  */
-type QueryResult<T extends ComponentBundle> = Query<T> & {
+type QueryResult<T extends ComponentBundle, B extends string> = Query<T, B> & {
 	/**
 	 * Returns an iterator that will skip any entities that also have the given components.
 	 *
@@ -177,7 +181,7 @@ type QueryResult<T extends ComponentBundle> = Query<T> & {
 	 * }
 	 * ```
 	 */
-	without: (this: Query<T>, ...components: DynamicBundle) => Query<T>;
+	without: (this: Query<T, B>, ...components: DynamicBundle) => Query<T, B>;
 	/**
 	 * Returns the next set of values from the query result. Once all results have been returned, the
 	 * QueryResult is exhausted and is no longer useful.
@@ -202,13 +206,13 @@ type QueryResult<T extends ComponentBundle> = Query<T> & {
 	 * @returns A LuaTuple of an entity followed with queried components
 	 * @See {@link Entity Entity}
 	 */
-	next: (this: Query<T>) => LuaTuple<[Entity<T>, ...T]>;
+	next: (this: Query<T, B>) => LuaTuple<[Entity<T, B>, ...T]>;
 	/**
 	 * Creates a "snapshot" of this query, draining this QueryResult and returning a list containing all of its results.
-	 * 
+	 *
 	 * By default, iterating over a QueryResult happens in "real time": it iterates over the actual data in the ECS, so
 	 * changes that occur during the iteration will affect future results.
-	 * 
+	 *
 	 * By contrast, `QueryResult:snapshot()` creates a list of all of the results of this query at the moment it is called,
 	 * so changes made while iterating over the result of `QueryResult:snapshot` do not affect future results of the
 	 * iteration.
@@ -220,7 +224,7 @@ type QueryResult<T extends ComponentBundle> = Query<T> & {
 	 * directly:
 	 * ```ts
 	 * for (const [entityId, health, player] of world.query(Health, Player).snapshot()) {
-     * 
+     *
 	 * }
 	 * However, the table itself is just a list of sub-tables structured like `[entityId, component1, component2, ...etc]`
 	 * @returns ReadonlyArray<[Entity<ComponentBundle>, ...ComponentBundle]>
@@ -228,7 +232,7 @@ type QueryResult<T extends ComponentBundle> = Query<T> & {
 
 	However, the table itself is just a list of sub-tables structured like `{entityId, component1, component2, ...etc}`.
 	 */
-	snapshot: (this: Query<T>) => ReadonlyArray<[Entity<T>, ...T]>;
+	snapshot: (this: Query<T, B>) => ReadonlyArray<[Entity<T, B>, ...T]>;
 };
 
 export type FilterOut<T extends Array<unknown>, F> = T extends [infer L, ...infer R]
